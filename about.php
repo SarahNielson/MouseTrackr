dc5a676d147b03c1a1b6e9448d0fe576fa93f155
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-03-21 12:07:05 -0600

    blah

--- about.php
+++ about.php
@@ -1,16 +1,3 @@
-<html>
-<head>
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-    <link rel="stylesheet" href="login.css">
-  </head>
-  <body>
-    <h1>Login Page</h1>
-    <form method="post" action="handler.php">
-      <div>Username: <input type="text" name="username"></div>
-      <div>Password: <input type="password" name="password"></div>
-      <div><input type="submit" value="Login"></div>
-    </form>
-  </body>
-
-
-</html>
+<?php 
+include_once("about.html");
+?>
