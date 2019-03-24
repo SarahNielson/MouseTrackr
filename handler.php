f3a3ee728473a8ba237aaee2dc9e5ac843ea5648
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-03-23 18:12:28 -0600

    m

--- handler.php
+++ handler.php
@@ -1,12 +1,12 @@
 <?php
   session_start();
   // Pretend i'm looking this up in a database
-  //$username_in_the_database = "abc123";
-  //$password_in_the_database = "abc123";
-  require_once "User.php";
-	$user1 = new User("sarah", "123abc");
-	$user2 = new User("other", "abc123");
-  if (($user2->password != $_POST["password"])) {
+  $username_in_the_database = "abc123";
+  $password_in_the_database = "abc123";
+  //require_once "User.php";
+	//$user1 = new User("sarah", "123abc");
+	//$user2 = new User("other", "abc123");
+  if (($password_in_the_database != $_POST["password"])||($username_in_the_database != $_POST["username"])) {
     $_SESSION['message'] = "Error: the information was incorrect.";
     header("Location: about.php");
     exit();
@@ -14,11 +14,3 @@
     $_SESSION['logged_in'] = true;
     header("Location: index.php");
   }
-//if (!($user2->isPasswordValid("abc123"))) {
- //	$_SESSION['message'] = "Error: the information was incorrect.";
-  //	header("Location: about.php");
-//	exit();
-//  } else {
-//	$_SESSION['logged_in'] = true;
- //	header("Location: index.php");
- // }
