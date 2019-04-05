7a5e4fc2350738ced90be1954a01a7f832bf9737
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-04-04 18:37:23 -0600

    login stuff

--- about.php
+++ about.php
@@ -37,28 +37,28 @@ session_start();
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true){
     echo 'You are already signed in, you can <a href="logout.php">sign out</a> if you want.';
-}else{
-echo '<form method="post" action="create_user.php">
+}else{ ?>
+<form method="post" action="create_user.php">
 <div><label id="create">New here? </label><input type="submit" value="Create New User"></div>
- </form>';
-}
-?>
+ </form>
+
 
 
  <form method="post" action="handler.php">
 <div><label for="email">Email:</label>
       <input value="<?php echo isset($_SESSION['form_input']['email']) ? $_SESSION['form_input']['email'] : ''; ?>" type="text" id="email" name="email"></div>
       <div>Password: <input type="password" id="password" name="password"></div>
-		<?php
+		
+      <div><input type="submit" value="Login"></div>
+    </form>
+<?php
+}
       		if (isset($_SESSION['message'])) {
                 $sentiment = (isset($_SESSION['good']) && ($_SESSION['good'])) ? "good" : "bad";
         	echo "<div class='" . $sentiment . "' id='message'>" . $_SESSION['message'] . "</div>";
      		 }
       		unset($_SESSION['message']);
      		 ?>
-      <div><input type="submit" value="Login"></div>
-    </form>
-
   </body>
 
 <?php require 'footer.html';?>
