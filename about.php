3d70b9c813826b32f425b070b7282a50198bac2e
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-04-04 18:41:54 -0600

    login stuff

--- about.php
+++ about.php
@@ -36,29 +36,23 @@ session_start();
 <?php
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(!(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true)){
-   // echo 'You are already signed in, you can <a href="logout.php">sign out</a> if you want.';
-//}else{ 
-?>
-<form method="post" action="create_user.php">
-<div><label id="create">New here? </label><input type="submit" value="Create New User"></div>
- </form>
-
-
-
- <form method="post" action="handler.php">
-<div><label for="email">Email:</label>
-      <input value="<?php echo isset($_SESSION['form_input']['email']) ? $_SESSION['form_input']['email'] : ''; ?>" type="text" id="email" name="email"></div>
+    echo 'You are already signed in, you can <a href="logout.php">sign out</a> if you want.';
+}else{ ?>
+	<form method="post" action="create_user.php">
+	<div><label id="create">New here? </label><input type="submit" value="Create New User"></div>
+ 	</form>
+	 <form method="post" action="handler.php">
+	<div><label for="email">Email:</label>
+      <input value="<?php echo isset($_SESSION['form_input']['email']) ? $_SESSION['form_input']['email'] : ''; ?>" type="text" 	id="email" name="email"></div>
       <div>Password: <input type="password" id="password" name="password"></div>
 		
       <div><input type="submit" value="Login"></div>
-    </form>
-<?php
-}else{
-echo 'You are already signed in, you can <a href="logout.php">sign out</a> if you want.';
+  	  </form>
+	<?php
 }
       		if (isset($_SESSION['message'])) {
-                $sentiment = (isset($_SESSION['good']) && ($_SESSION['good'])) ? "good" : "bad";
-        	echo "<div class='" . $sentiment . "' id='message'>" . $_SESSION['message'] . "</div>";
+                	$sentiment = (isset($_SESSION['good']) && ($_SESSION['good'])) ? "good" : "bad";
+        		echo "<div class='" . $sentiment . "' id='message'>" . $_SESSION['message'] . "</div>";
      		 }
       		unset($_SESSION['message']);
      		 ?>
