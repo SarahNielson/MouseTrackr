ffcace21ebc43bd94d712d73119ebceccfd84e2f
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-04-18 10:34:50 -0600

    forum jquery

--- forum.php
+++ forum.php
@@ -11,6 +11,9 @@ require_once 'Dao.php';
     <link rel="stylesheet" href="style.css"> 
 <link rel="stylesheet" href="comments.css"> 
 <script src="js/comments.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+<script src="js/ajax.js"></script>
 <title>MouseTrackr</title> </head>
 
 <nav>
