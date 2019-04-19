c650fd1a508501817a4d5e8359a3ab8474e25f04
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-04-19 00:10:13 -0600

    forum

--- comment_handler.php
+++ comment_handler.php
@@ -29,6 +29,6 @@ $dao->saveComment($comment);
 $_SESSION["logged_in"] = true;         
 $_SESSION['message'] = "Thanks for posting!";
 $_SESSION['good'] = true;
-$_SESSION['comment'] = "";
+unset ($_SESSION['form_input']);
 header('Location: forum.php');
 exit;
