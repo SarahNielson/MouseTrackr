247d037d0a9db267ba34acb1cd6fdc61364e18a4
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-04-20 14:34:29 -0600

    jquery

--- create_user.php
+++ create_user.php
@@ -23,7 +23,7 @@ session_start();
       <?php
       if (isset($_SESSION['messages'])) {
         foreach($_SESSION['messages'] as $message) {
-          echo "<div class='bad' id="message"><span class='close'>X</span>{$message}</div>";
+          echo "<div class='bad' id='message'><span class='close'>X</span>{$message}</div>";
         }
       }
       unset($_SESSION['messages']);
