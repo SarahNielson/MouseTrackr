d2b59b1e5ae2aa008bdf0806842abc7aa4d2af7e
Sarah Nielson <SarahNielson391@u.boisestate.edu>
2019-04-20 13:58:58 -0600

    style

--- create_user.php
+++ create_user.php
@@ -9,7 +9,7 @@ session_start();
     <link rel="stylesheet" href="createustyle.css">
   </head>
   <body>
-    <h1>Create a User</h1>
+    <h1>New User Sign-Up:</h1>
     <form method="post" action="create_user_handler.php">
       <div><label for="username">Username:</label>
         <input value="<?php echo isset($_SESSION['form_input']['username']) ? $_SESSION['form_input']['username'] : ''; ?>" type="text" id="username" name="username"></div>
@@ -27,7 +27,7 @@ session_start();
       unset($_SESSION['message']);
       unset($_SESSION['form_input']);
       ?>
-      <div><input type="submit" value="Create User"></div>
+      <div><input type="submit" value="Create Account"></div>
     </form>
 <?php
 // require 'footer.html';
